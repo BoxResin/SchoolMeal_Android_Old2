9ba5950a87ed7aab5f5437344dc4922e3c0f0480
Minsuk Eom <boxresin@gmail.com>
2018-09-22 23:34:51 +0900

    MockSchoolApi#search 에 2초 딜레이 줌

--- app/src/main/java/winapi251/app/schoolmeal/model/school/MockSchoolApi.kt
+++ app/src/main/java/winapi251/app/schoolmeal/model/school/MockSchoolApi.kt
@@ -1,18 +1,26 @@
 package winapi251.app.schoolmeal.model.school
 
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.IO
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.withContext
 import winapi251.app.schoolmeal.model.Area
 
 /** 가짜 학교 API */
 object MockSchoolApi : SchoolApi
 {
-	override suspend fun search(name: String): List<School> = listOf(
-			School(code = "B100000925", courseCode = "2", name = "서울선린초등학교",
-					address = "서울특별시 강동구 진황도로61길 29", area = Area.SEN),
+	override suspend fun search(name: String): List<School> = withContext(Dispatchers.IO) {
+		delay(2000)
 
-			School(code = "B100002350", courseCode = "3", name = "선린중학교",
-					address = "서울특별시 용산구 원효로97길 33-4", area = Area.SEN),
+		listOf(
+				School(code = "B100000925", courseCode = "2", name = "서울선린초등학교",
+						address = "서울특별시 강동구 진황도로61길 29", area = Area.SEN),
 
-			School(code = "B100000658", courseCode = "4", name = "선린인터넷고등학교",
-					address = "서울특별시 용산구 원효로97길 33-4", area = Area.SEN)
-	)
+				School(code = "B100002350", courseCode = "3", name = "선린중학교",
+						address = "서울특별시 용산구 원효로97길 33-4", area = Area.SEN),
+
+				School(code = "B100000658", courseCode = "4", name = "선린인터넷고등학교",
+						address = "서울특별시 용산구 원효로97길 33-4", area = Area.SEN)
+		)
+	}
 }
