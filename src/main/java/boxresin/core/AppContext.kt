df49844f9dc1e4e2a7494eb0f116242d6c8a2a91
Minsuk Eom <boxresin@gmail.com>
2018-10-12 18:59:58 +0900

    애플리케이션 문맥 인터페이스 추가 #1

---
+++ src/main/java/boxresin/core/AppContext.kt
@@ -0,0 +1,6 @@
+package boxresin.core
+
+/** 애플리케이션 문맥 */
+interface AppContext
+{
+}
---
+++ src/main/java/boxresin/core/android/AndroidAppContext.kt
@@ -0,0 +1,8 @@
+package boxresin.core.android
+
+import boxresin.core.AppContext
+
+/** 안드로이드 애플리케이션 문맥 */
+class AndroidAppContext : AppContext
+{
+}
